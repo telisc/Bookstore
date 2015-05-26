d5650ed049b889dabed87e178fbfec3fcaa9faf3
telisc <telisck@yahoo.com>
2015-05-26 07:16:33 -0700

    jquery ui

--- Bookstore/App_Start/BundleConfig.cs
+++ Bookstore/App_Start/BundleConfig.cs
@@ -14,6 +14,12 @@ namespace Bookstore
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            //telis
+            bundles.Add(new ScriptBundle("~/bundles/jquery.ui").Include(
+                        "~/Scripts/jquery.ui*"));
+
+            //end telis
+            
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
